9bce9e71f30e408e4559ab357af217cd54d8a535
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-11-03 19:35:09 +0530

    Create main functions

---
+++ index.js
@@ -0,0 +1,11 @@
+const login = require('./lib/login');
+const logout = require('./lib/logout');
+const getData = require('./lib/getData');
+const verify = require('./lib/verify');
+
+module.exports = {
+  login,
+  logout,
+  getData,
+  verify,
+};
