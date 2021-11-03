7ac6c5bb14f5211f11cb3f906f0decd40d4b1e68
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-11-03 21:17:18 +0530

    intialize example

---
+++ example/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const app = () => {
+    return <div>Hello</div>;
+};
+
+export default app;
---
+++ example/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        }),
+    ],
+};
